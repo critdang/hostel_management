a641570afdeb41c783099f746e9a3c2e90394889
Dang Quoc Huy <critdang1010@gmail.com>
2021-11-23 23:02:48 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// Login pages
+let container = document.getElementById('container');
+
+toggle = () => {
+    container.classList.toggle('sign-in')
+    container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+    container.classList.add('sign-in')
+},200)
+// Scroll menu
+
---
+++ js/slider.js
@@ -0,0 +1,50 @@
+var slideIndex = 0;
+showSlides();
+var slides,dots;
+console.log(slides.length)
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("slide__item");
+    console.log(slides)
+    console.log(dots)
+    dots = document.getElementsByClassName("slide__dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    console.log(dots[slideIndex-1].className)
+    setTimeout(showSlides, 2000); // Change image every 8 seconds
+}
+
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function currentSlide(index) {
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";  
+    dots[index-1].className += " active";
+}
