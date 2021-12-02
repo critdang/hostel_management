a35e27d54360f686799a3f3189b09db3fd462f81
Quang Dai <zekromdb@gmail.com>
2021-12-02 15:38:14 +0700

    dai fix

--- views/CheckoutView.php
+++ views/CheckoutView.php
@@ -1,6 +1,6 @@
 <?php
     class CheckoutView {
-        public function showAllRoom($rooms) {
+        public function showAllRoom($rooms,$total) {
             require_once('templates/Checkout.php');
         }
     }
