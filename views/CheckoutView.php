4476c5f6f64b877527f9fcd57cc6eeae89ef035f
Dang Quoc Huy <critdang1010@gmail.com>
2021-12-02 21:26:46 +0700

    fix checkbox view

--- views/CheckoutView.php
+++ views/CheckoutView.php
@@ -1,7 +1,8 @@
 <?php
     class CheckoutView {
         public function showAllRoom($rooms) {
-            require_once('templates/Checkout.php');
+            // require_once('templates/Checkout.php');
+            require_once('include/Checkout.php');
         }
     }
 
