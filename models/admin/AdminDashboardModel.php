19fd64775b469c8ee305b1be68b2323d7fa67d3c
Dang Quoc Huy <critdang1010@gmail.com>
2021-12-08 14:03:47 +0700

    huy upload

--- models/admin/AdminDashboardModel.php
+++ models/admin/AdminDashboardModel.php
@@ -26,5 +26,30 @@
             // bước 5 trả về controller
             return $products;
         }
+
+        public function getAllOrder() {
+            $con = new mysqli("localhost","root","","hostel");
+            
+            // Check connection
+            if ($con -> connect_errno) {
+                echo "Failed to connect to MySQL: " . $con -> connect_error;
+            }
+        //    tạo ra các con trỏ
+            $result = $con -> query("SELECT * FROM tbl_room,tbl_category WHERE tbl_room.category_id=tbl_category.category_id");
+
+            $orders = array();
+
+            if($result->num_rows>0) {
+                // Trả về từng room
+                while($order = mysqli_fetch_array($result)) {
+                    // lưu ptu vào posts
+                    $orders[] = $order;
+                }
+            }
+            // bước 5 trả về controller
+            return $orders;
+
+        }
     }
+
 ?>
--- views/AdminView.php
+++ views/AdminView.php
@@ -3,6 +3,9 @@
         public function showDashBoard($products) {
             require_once('../../templates/admin/Admin.php');
         }
+        public function showOrder($orders) {
+            require_once('../../templates/admin/Admin.php');
+        }
     }
 
 ?>
